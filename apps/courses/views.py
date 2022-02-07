9946bae8ed2902b3f0169dea8e34b730aa42d518
Bert <wyx20000905@163.com>
2022-02-08 00:31:02 +0800

    write course-video page

--- apps/courses/views.py
+++ apps/courses/views.py
@@ -3,8 +3,49 @@ from django.views.generic.base import View
 from pure_pagination import Paginator, PageNotAnInteger
 from django.contrib.auth.mixins import LoginRequiredMixin
 
-from apps.courses.models import Course, CourseTag, CourseResource
+from apps.courses.models import Course, CourseTag, CourseResource, Video
 from apps.operations.models import UserFavorite, UserCourse, CourseComments
+
+class VideoView(View):
+    def get(self, request, course_id, video_id, *args, **kwargs):
+        """
+        获取章节视频信息
+        """
+        course = Course.objects.get(id=int(course_id))
+        course.click_nums += 1
+        course.save()
+
+        video = Video.objects.get(id=int(video_id))
+
+        #查询用户是否已经关联了该课程
+        user_courses = UserCourse.objects.filter(user=request.user, course=course)
+        if not user_courses:
+            user_course = UserCourse(user=request.user, course=course)
+            user_course.save()
+
+            course.students += 1
+            course.save()
+
+        #学习过该课程的所有同学
+        user_courses = UserCourse.objects.filter(course=course)
+        user_ids = [user_course.user.id for user_course in user_courses]
+        all_courses = UserCourse.objects.filter(user_id__in=user_ids).order_by("-course__click_nums")[:5]
+        # related_courses = [user_course.course for user_course in all_courses if user_course.course.id != course.id]
+        related_courses = []
+        for item in all_courses:
+            if item.course.id != course.id:
+                related_courses.append(item.course)
+
+        course_resources = CourseResource.objects.filter(course=course)
+
+
+        return render(request, "course-play.html", {
+            "course": course,
+            "course_resources":course_resources,
+            "related_courses":related_courses,
+            "video":video,
+        })
+
 
 class CourseCommentsView(LoginRequiredMixin, View):
     login_url = "/login/"
